e4c15feb6fa2e70ef7b4c56b4711f590ea2958e8
bhutianimukul <bhutianimukul@gmail.com>
2021-07-29 17:11:09 +0530

    improved code for categories

--- newsapp/lib/main.dart
+++ newsapp/lib/main.dart
@@ -36,18 +36,7 @@ class _MyAppState extends State<MyApp> {
     return ChangeNotifierProvider(
       create: (_) => NewsProvider(),
       child: MaterialApp(routes: {
-        Business.routeName: (_) => Business(),
         Items.routeName: (_) => Items(),
-        Tesla.routeName: (_) => Tesla(),
-        Apple.routeName: (_) => Apple(),
-        GlobalNews.routeName: (_) => GlobalNews(),
-        Technology.routeName: (_) => Technology(),
-        Journal.routeName: (_) => Journal(),
-        Sports.routeName: (_) => Sports(),
-        Health.routeName: (_) => Health(),
-        Science.routeName: (_) => Science(),
-        Entertainment.routeName: (_) => Entertainment(),
-        Bitcoin.routeName: (_) => Bitcoin(),
         FrontScreen.routeName: (_) => FrontScreen(),
         NewsDetailScreen.routeName: (_) => NewsDetailScreen(),
         LoginScreen.routeName: (_) => LoginScreen(),
